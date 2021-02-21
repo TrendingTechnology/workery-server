a2b10f4c37a17d57023a65174a5baa4e1d4d23a8
bartmika <bart@mikasoftware.com>
2021-02-21 16:41:50 -0500

    Latest progress with the database schema for the workery server.

--- scripts/migrations/0001_initial_down.sql
+++ scripts/migrations/0001_initial_down.sql
@@ -1,2 +1,35 @@
+-- TODO: Staff
+-- TODO: Activity Sheet Items
+-- TODO: Ongoing Work
+-- TODO: Partner
+-- TODO: Private File Upload
+-- TODO: Private Image Upload
+-- TODO: Public Image Upload
+-- TODO: Task Items
+-- TODO: Unified Search Item
+DROP TABLE work_order_deposits CASCADE;
+DROP TABLE work_order_invoices CASCADE;
+DROP TABLE work_order_comments CASCADE;
+DROP TABLE work_order_skill_sets CASCADE;
+DROP TABLE work_order_tags CASCADE;
+DROP TABLE work_orders CASCADE;
+DROP TABLE associate_insurance_requirements CASCADE;
+DROP TABLE associate_comments CASCADE;
+DROP TABLE associate_tags CASCADE;
+DROP TABLE associate_skill_sets CASCADE;
+DROP TABLE associate_vehicle_types CASCADE;
+DROP TABLE associate CASCADE;
+DROP TABLE customer_comments CASCADE;
+DROP TABLE customer_tags CASCADE;
+DROP TABLE customers CASCADE;
+DROP TABLE bulletin_board_items CASCADE;
+DROP TABLE work_order_service_fees CASCADE;
+DROP TABLE comments CASCADE;
+DROP TABLE vehicle_types CASCADE;
+DROP TABLE tags CASCADE;
+DROP TABLE skill_set_insurance_requirements CASCADE;
+DROP TABLE skill_sets CASCADE;
+DROP TABLE how_hear_about_us_items CASCADE;
+DROP TABLE insurance_requirements CASCADE;
 DROP TABLE users CASCADE;
 DROP TABLE tenants CASCADE;
