d40bf7c2590ddfa0d7bb594b6e9c216fe3137bd5
bartmika <bart@mikasoftware.com>
2021-06-14 00:29:35 -0400

    Moved from `sql` to `null` package for the `Staff` models.

--- cmd/etl_staff.go
+++ cmd/etl_staff.go
@@ -80,67 +80,67 @@ func doRunImportStaff() {
 type OldUStaff struct {
 	Created                 time.Time       `json:"created"`
 	LastModified            time.Time       `json:"last_modified"`
-	AvailableLanguage       sql.NullString  `json:"available_language"`
-	ContactType             sql.NullString  `json:"contact_type"`
-	Email                   sql.NullString  `json:"email"`
-	FaxNumber               sql.NullString  `json:"fax_number"`
-	Telephone               sql.NullString  `json:"telephone"`
+	AvailableLanguage       null.String  `json:"available_language"`
+	ContactType             null.String  `json:"contact_type"`
+	Email                   null.String  `json:"email"`
+	FaxNumber               null.String  `json:"fax_number"`
+	Telephone               null.String  `json:"telephone"`
 	TelephoneTypeOf         int8            `json:"telephone_type_of"`
-	TelephoneExtension      sql.NullString  `json:"telephone_extension"`
-	OtherTelephone          sql.NullString  `json:"other_telephone"`
-	OtherTelephoneExtension sql.NullString  `json:"other_telephone_extension"`
+	TelephoneExtension      null.String  `json:"telephone_extension"`
+	OtherTelephone          null.String  `json:"other_telephone"`
+	OtherTelephoneExtension null.String  `json:"other_telephone_extension"`
 	OtherTelephoneTypeOf    int8            `json:"other_telephone_type_of"`
 	AddressCountry          string          `json:"address_country"`
 	AddressLocality         string          `json:"address_locality"`
 	AddressRegion           string          `json:"address_region"`
-	PostOfficeBoxNumber     sql.NullString  `json:"post_office_box_number"`
-	PostalCode              sql.NullString  `json:"postal_code"`
+	PostOfficeBoxNumber     null.String  `json:"post_office_box_number"`
+	PostalCode              null.String  `json:"postal_code"`
 	StreetAddress           string          `json:"street_address"`
-	StreetAddressExtra      sql.NullString  `json:"street_address_extra"`
-	Elevation               sql.NullFloat64 `json:"elevation"`
-	Latitude                sql.NullFloat64 `json:"latitude"`
-	Longitude               sql.NullFloat64 `json:"longitude"`
-	GivenName               sql.NullString  `json:"given_name"`
-	MiddleName              sql.NullString  `json:"middle_name"`
-	LastName                sql.NullString  `json:"last_name"`
-	Birthdate               sql.NullTime    `json:"birthdate"`
-	JoinDate                sql.NullTime    `json:"join_date"`
-	Nationality             sql.NullString  `json:"nationality"`
-	Gender                  sql.NullString  `json:"gender"`
-	TaxId                   sql.NullString  `json:"tax_id"`
+	StreetAddressExtra      null.String  `json:"street_address_extra"`
+	Elevation               null.Float `json:"elevation"`
+	Latitude                null.Float `json:"latitude"`
+	Longitude               null.Float `json:"longitude"`
+	GivenName               null.String  `json:"given_name"`
+	MiddleName              null.String  `json:"middle_name"`
+	LastName                null.String  `json:"last_name"`
+	Birthdate               null.Time    `json:"birthdate"`
+	JoinDate                null.Time    `json:"join_date"`
+	Nationality             null.String  `json:"nationality"`
+	Gender                  null.String  `json:"gender"`
+	TaxId                   null.String  `json:"tax_id"`
 	Id                      uint64          `json:"id"`
-	IndexedText             sql.NullString  `json:"indexed_text"`
-	CreatedFrom              sql.NullString `json:"created_from"`
+	IndexedText             null.String  `json:"indexed_text"`
+	CreatedFrom              null.String `json:"created_from"`
 	CreatedFromIsPublic      bool           `json:"created_from_is_public"`
-	LastModifiedFrom         sql.NullString `json:"last_modified_from"`
+	LastModifiedFrom         null.String `json:"last_modified_from"`
 	LastModifiedFromIsPublic bool           `json:"last_modified_from_is_public"`
 	IsArchived               bool           `json:"is_archived"`
-	CreatedById              sql.NullInt64  `json:"created_by_id"`
-	LastModifiedById         sql.NullInt64  `json:"last_modified_by_id"`
-	OwnerId      sql.NullInt64  `json:"owner_id"`
-	HowHearOther sql.NullString `json:"how_hear_other"`
-	HowHearId sql.NullInt64 `json:"how_hear_id"`
-	AvatarImageId                        sql.NullInt64  `json:"avatar_image_id"`
-	PersonalEmail                        sql.NullString `json:"personal_email"`
-	EmergencyContactAlternativeTelephone sql.NullString `json:"emergency_contact_alternative_telephone"`
-	EmergencyContactName                 sql.NullString `json:"emergency_contact_name"`
-	EmergencyContactRelationship         sql.NullString `json:"emergency_contact_relationship"`
-	EmergencyContactTelephone            sql.NullString `json:"emergency_contact_telephone"`
-	PoliceCheck                          sql.NullTime   `json:"police_check"`
+	CreatedById              null.Int  `json:"created_by_id"`
+	LastModifiedById         null.Int  `json:"last_modified_by_id"`
+	OwnerId      null.Int  `json:"owner_id"`
+	HowHearOther null.String `json:"how_hear_other"`
+	HowHearId null.Int `json:"how_hear_id"`
+	AvatarImageId                        null.Int  `json:"avatar_image_id"`
+	PersonalEmail                        null.String `json:"personal_email"`
+	EmergencyContactAlternativeTelephone null.String `json:"emergency_contact_alternative_telephone"`
+	EmergencyContactName                 null.String `json:"emergency_contact_name"`
+	EmergencyContactRelationship         null.String `json:"emergency_contact_relationship"`
+	EmergencyContactTelephone            null.String `json:"emergency_contact_telephone"`
+	PoliceCheck                          null.Time   `json:"police_check"`
 	// TypeOf                   int8            `json:"type_of"`
 	// IsOkToEmail              bool            `json:"is_ok_to_email"`
 	// IsOkToText               bool            `json:"is_ok_to_text"`
 	// IsBusiness               bool            `json:"is_business"`
 	// IsSenior                 bool            `json:"is_senior"`
 	// IsSupport                bool            `json:"is_support"`
-	// JobInfoRead              sql.NullString  `json:"job_info_read"`
-	// OrganizationId           sql.NullInt64   `json:"organization_id"`
+	// JobInfoRead              null.String  `json:"job_info_read"`
+	// OrganizationId           null.Int   `json:"organization_id"`
 	// IsBlacklisted            bool            `json:"is_blacklisted"`
 	// DeactivationReason       int8            `json:"deactivation_reason"`
 	// DeactivationReasonOther  string          `json:"deactivation_reason_other"`
 	// State                    string          `json:"state"`
 	// HowHearOld               int8            `json:"how_hear_old"`
-	// OrganizationName         sql.NullString  `json:"organization_name"`
+	// OrganizationName         null.String  `json:"organization_name"`
 	// OrganizationTypeOf       int8            `json:"organization_type_of"`
 }
 
--- internal/models/staff.go
+++ internal/models/staff.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"context"
-	"database/sql"
 	"time"
 
 	null "gopkg.in/guregu/null.v4"
@@ -32,53 +31,53 @@ import (
 type Staff struct {
 	CreatedTime                          time.Time       `json:"created_time"`
 	LastModifiedTime                     time.Time       `json:"last_modified_time"`
-	AvailableLanguage                    sql.NullString  `json:"available_language"`
-	ContactType                          sql.NullString  `json:"contact_type"`
-	Email                                sql.NullString  `json:"email"`
-	FaxNumber                            sql.NullString  `json:"fax_number"`
-	Telephone                            sql.NullString  `json:"telephone"`
+	AvailableLanguage                    null.String  `json:"available_language"`
+	ContactType                          null.String  `json:"contact_type"`
+	Email                                null.String  `json:"email"`
+	FaxNumber                            null.String  `json:"fax_number"`
+	Telephone                            null.String  `json:"telephone"`
 	TelephoneTypeOf                      int8            `json:"telephone_type_of"`
-	TelephoneExtension                   sql.NullString  `json:"telephone_extension"`
-	OtherTelephone                       sql.NullString  `json:"other_telephone"`
-	OtherTelephoneExtension              sql.NullString  `json:"other_telephone_extension"`
+	TelephoneExtension                   null.String  `json:"telephone_extension"`
+	OtherTelephone                       null.String  `json:"other_telephone"`
+	OtherTelephoneExtension              null.String  `json:"other_telephone_extension"`
 	OtherTelephoneTypeOf                 int8            `json:"other_telephone_type_of"`
 	AddressCountry                       string          `json:"address_country"`
 	AddressLocality                      string          `json:"address_locality"`
 	AddressRegion                        string          `json:"address_region"`
-	PostOfficeBoxNumber                  sql.NullString  `json:"post_office_box_number"`
-	PostalCode                           sql.NullString  `json:"postal_code"`
+	PostOfficeBoxNumber                  null.String  `json:"post_office_box_number"`
+	PostalCode                           null.String  `json:"postal_code"`
 	StreetAddress                        string          `json:"street_address"`
-	StreetAddressExtra                   sql.NullString  `json:"street_address_extra"`
-	Elevation                            sql.NullFloat64 `json:"elevation"`
-	Latitude                             sql.NullFloat64 `json:"latitude"`
-	Longitude                            sql.NullFloat64 `json:"longitude"`
-	GivenName                            sql.NullString  `json:"given_name"`
-	MiddleName                           sql.NullString  `json:"middle_name"`
-	LastName                             sql.NullString  `json:"last_name"`
-	Birthdate                            sql.NullTime    `json:"birthdate"`
-	JoinDate                             sql.NullTime    `json:"join_date"`
-	Nationality                          sql.NullString  `json:"nationality"`
-	Gender                               sql.NullString  `json:"gender"`
-	TaxId                                sql.NullString  `json:"tax_id"`
+	StreetAddressExtra                   null.String  `json:"street_address_extra"`
+	Elevation                            null.Float `json:"elevation"`
+	Latitude                             null.Float `json:"latitude"`
+	Longitude                            null.Float `json:"longitude"`
+	GivenName                            null.String  `json:"given_name"`
+	MiddleName                           null.String  `json:"middle_name"`
+	LastName                             null.String  `json:"last_name"`
+	Birthdate                            null.Time    `json:"birthdate"`
+	JoinDate                             null.Time    `json:"join_date"`
+	Nationality                          null.String  `json:"nationality"`
+	Gender                               null.String  `json:"gender"`
+	TaxId                                null.String  `json:"tax_id"`
 	TenantId                             uint64          `json:"tenant_id"`
 	Id                                   uint64          `json:"id"`
 	Uuid                                 string          `json:"uuid"`
-	IndexedText                          sql.NullString  `json:"indexed_text"`
-	CreatedFromIP                        sql.NullString  `json:"created_from_ip"`
-	LastModifiedFromIP                   sql.NullString  `json:"last_modified_from_ip"`
+	IndexedText                          null.String  `json:"indexed_text"`
+	CreatedFromIP                        null.String  `json:"created_from_ip"`
+	LastModifiedFromIP                   null.String  `json:"last_modified_from_ip"`
 	State                                int8            `json:"state"`
 	CreatedById                          null.Int        `json:"created_by_id"`
 	LastModifiedById                     null.Int        `json:"last_modified_by_id"`
 	UserId                               uint64          `json:"user_id"`
-	HowHearOther                         sql.NullString  `json:"how_hear_other"`
-	HowHearId                            sql.NullInt64   `json:"how_hear_id"`
-	AvatarImageId                        sql.NullInt64   `json:"avatar_image_id"`
-	PersonalEmail                        sql.NullString  `json:"personal_email"`
-	EmergencyContactAlternativeTelephone sql.NullString  `json:"emergency_contact_alternative_telephone"`
-	EmergencyContactName                 sql.NullString  `json:"emergency_contact_name"`
-	EmergencyContactRelationship         sql.NullString  `json:"emergency_contact_relationship"`
-	EmergencyContactTelephone            sql.NullString  `json:"emergency_contact_telephone"`
-	PoliceCheck                          sql.NullTime    `json:"police_check"`
+	HowHearOther                         null.String  `json:"how_hear_other"`
+	HowHearId                            null.Int   `json:"how_hear_id"`
+	AvatarImageId                        null.Int   `json:"avatar_image_id"`
+	PersonalEmail                        null.String  `json:"personal_email"`
+	EmergencyContactAlternativeTelephone null.String  `json:"emergency_contact_alternative_telephone"`
+	EmergencyContactName                 null.String  `json:"emergency_contact_name"`
+	EmergencyContactRelationship         null.String  `json:"emergency_contact_relationship"`
+	EmergencyContactTelephone            null.String  `json:"emergency_contact_telephone"`
+	PoliceCheck                          null.Time    `json:"police_check"`
 	OldId                                uint64          `json:"old_id"`
 	IsOkToEmail                          bool            `json:"is_ok_to_email"`
 	IsOkToText                           bool            `json:"is_ok_to_text"`
